d97784d21782c1ca5ecc137a7467ccb41683b0fa
ojimfp <mfauzanp@gmail.com>
2020-06-22 13:57:45 +0700

    Setup template

---
+++ application/views/_partials/js.php
@@ -0,0 +1,12 @@
+<!-- Partial berisi kode-kode Javascript -->
+
+<script src="assets/js/jquery-3.2.1.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/jquery.slimscroll.js"></script>
+<script src="assets/js/select2.min.js"></script>
+<script src="assets/js/jquery.dataTables.min.js"></script>
+<script src="assets/js/dataTables.bootstrap4.min.js"></script>
+<script src="assets/js/moment.min.js"></script>
+<script src="assets/js/bootstrap-datetimepicker.min.js"></script>
+<script src="assets/js/app.js"></script>
