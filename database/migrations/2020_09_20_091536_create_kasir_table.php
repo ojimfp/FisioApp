dc05818cb558a6651861e3446544baac86127d21
ojimfp <mfauzanp@gmail.com>
2020-09-20 17:04:35 +0700

    Revisi RekamMedisController dan table kasir

---
+++ database/migrations/2020_09_20_091536_create_kasir_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKasirTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kasir', function (Blueprint $table) {
+            $table->id();
+            $table->string('pasien_id');
+            $table->date('tgl_pembayaran');
+            $table->string('tipe_pembayaran');
+            $table->bigInteger('total_biaya');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kasir');
+    }
+}
