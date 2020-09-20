86b6139c4f595ffe121aafdaf1c10ab696873479
ojimfp <mfauzanp@gmail.com>
2020-09-20 17:09:32 +0700

    Revisi view rekam medis

--- resources/views/riwayat_pasien.blade.php
+++ resources/views/riwayat_pasien.blade.php
@@ -171,6 +171,7 @@
                                                             <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
                                                             <div class="dropdown-menu dropdown-menu-right">
                                                                 <a class="dropdown-item" href="{{ route('rekam-medis.edit', $rm->id) }}"><i class="fa fa-pencil m-r-5"></i> Edit</a>
+                                                                <a class="dropdown-item" href="{{ route('rekam-medis.edit', $rm->id) }}"><i class="fa fa-pencil m-r-5"></i> Edit</a>
                                                                 <a class="dropdown-item" href="javascript:;" data-toggle="modal" onclick="deleteData('{{ $rm->id }}')" data-target="#delete_rm"><i class="fa fa-trash-o m-r-5"></i> Hapus</a>
                                                             </div>
                                                         </div>
@@ -204,89 +205,89 @@
                         <div class="tab-pane" id="rekam-kasir">
                             <div class="tab-content">
                                 <div class="tab-pane show active" id="rekam-medis">
-                                <div class="row">
+                                    <div class="row">
                                         <div class="col-md-12">
                                             <div class="table-responsive">
                                                 <table class="table table-border table-striped custom-table datatable mb-0">
-                                                <thead>
-                                                    <tr>
-                                                        <th>#</th>
-                                                        <th>No. Tagihan</th>
-                                                        <th>Nama Pasien</th>
-                                                        <th>Tanggal Tagihan</th>
-                                                        <th>Tanggal Pembayaran</th>
-                                                        <th>Tipe Pembayaran </th>
-                                                        <th>Total Biaya</th>
-                                                        <th>Status</th>
-                                                        <th class="text-right">Opsi</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                        <td>1</td>
-                                                        <td><a href="invoice-view.html">#INV-0001</a></td>
-                                                        <td>Charles Ortega</td>
-                                                        <td>1 Aug 2018</td>
-                                                        <td>7 Aug 2018</td>
-                                                        <td>Tunai</td>
-                                                        <td>$20</td>
-                                                        <td><span class="custom-badge status-green">Paid</span></td>
-                                                        <td class="text-right">
-                                                            <div class="dropdown dropdown-action">
-                                                                <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
-                                                                <div class="dropdown-menu dropdown-menu-right">
-                                                                    <a class="dropdown-item" href="edit-invoice.html"><i class="fa fa-pencil m-r-5"></i> Edit</a>
-                                                                    <a class="dropdown-item" href="invoice-view.html"><i class="fa fa-eye m-r-5"></i> View</a>
-                                                                    <a class="dropdown-item" href="#"><i class="fa fa-file-pdf-o m-r-5"></i> Download</a>
-                                                                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_invoice"><i class="fa fa-trash-o m-r-5"></i> Delete</a>
+                                                    <thead>
+                                                        <tr>
+                                                            <th>#</th>
+                                                            <th>No. Tagihan</th>
+                                                            <th>Nama Pasien</th>
+                                                            <th>Tanggal Tagihan</th>
+                                                            <th>Tanggal Pembayaran</th>
+                                                            <th>Tipe Pembayaran </th>
+                                                            <th>Total Biaya</th>
+                                                            <th>Status</th>
+                                                            <th class="text-right">Opsi</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>1</td>
+                                                            <td><a href="invoice-view.html">#INV-0001</a></td>
+                                                            <td>Charles Ortega</td>
+                                                            <td>1 Aug 2018</td>
+                                                            <td>7 Aug 2018</td>
+                                                            <td>Tunai</td>
+                                                            <td>$20</td>
+                                                            <td><span class="custom-badge status-green">Paid</span></td>
+                                                            <td class="text-right">
+                                                                <div class="dropdown dropdown-action">
+                                                                    <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
+                                                                    <div class="dropdown-menu dropdown-menu-right">
+                                                                        <a class="dropdown-item" href="edit-invoice.html"><i class="fa fa-pencil m-r-5"></i> Edit</a>
+                                                                        <a class="dropdown-item" href="invoice-view.html"><i class="fa fa-eye m-r-5"></i> View</a>
+                                                                        <a class="dropdown-item" href="#"><i class="fa fa-file-pdf-o m-r-5"></i> Download</a>
+                                                                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_invoice"><i class="fa fa-trash-o m-r-5"></i> Delete</a>
+                                                                    </div>
                                                                 </div>
-                                                            </div>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>2</td>
-                                                        <td><a href="invoice-view.html">#INV-0002</a></td>
-                                                        <td>Denise Stevens</td>
-                                                        <td>24 Aug 2018</td>
-                                                        <td>24 Aug 2018</td>
-                                                        <td>Kartu</td>
-                                                        <td>$6</td>
-                                                        <td><span class="custom-badge status-blue">Sent</span></td>
-                                                        <td class="text-right">
-                                                            <div class="dropdown dropdown-action">
-                                                                <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
-                                                                <div class="dropdown-menu dropdown-menu-right">
-                                                                    <a class="dropdown-item" href="edit-invoice.html"><i class="fa fa-pencil m-r-5"></i> Edit</a>
-                                                                    <a class="dropdown-item" href="invoice-view.html"><i class="fa fa-eye m-r-5"></i> View</a>
-                                                                    <a class="dropdown-item" href="#"><i class="fa fa-file-pdf-o m-r-5"></i> Download</a>
-                                                                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_invoice"><i class="fa fa-trash-o m-r-5"></i> Delete</a>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>2</td>
+                                                            <td><a href="invoice-view.html">#INV-0002</a></td>
+                                                            <td>Denise Stevens</td>
+                                                            <td>24 Aug 2018</td>
+                                                            <td>24 Aug 2018</td>
+                                                            <td>Kartu</td>
+                                                            <td>$6</td>
+                                                            <td><span class="custom-badge status-blue">Sent</span></td>
+                                                            <td class="text-right">
+                                                                <div class="dropdown dropdown-action">
+                                                                    <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
+                                                                    <div class="dropdown-menu dropdown-menu-right">
+                                                                        <a class="dropdown-item" href="edit-invoice.html"><i class="fa fa-pencil m-r-5"></i> Edit</a>
+                                                                        <a class="dropdown-item" href="invoice-view.html"><i class="fa fa-eye m-r-5"></i> View</a>
+                                                                        <a class="dropdown-item" href="#"><i class="fa fa-file-pdf-o m-r-5"></i> Download</a>
+                                                                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_invoice"><i class="fa fa-trash-o m-r-5"></i> Delete</a>
+                                                                    </div>
                                                                 </div>
-                                                            </div>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>3</td>
-                                                        <td><a href="invoice-view.html">#INV-0003</a></td>
-                                                        <td>Dennis Salazar</td>
-                                                        <td>1 Sep 2018</td>
-                                                        <td>7 Sep 2018</td>
-                                                        <td>Tunai</td>
-                                                        <td>$20</td>
-                                                        <td><span class="custom-badge status-orange">Partially Paid</span></td>
-                                                        <td class="text-right">
-                                                            <div class="dropdown dropdown-action">
-                                                                <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
-                                                                <div class="dropdown-menu dropdown-menu-right">
-                                                                    <a class="dropdown-item" href="edit-invoice.html"><i class="fa fa-pencil m-r-5"></i> Edit</a>
-                                                                    <a class="dropdown-item" href="invoice-view.html"><i class="fa fa-eye m-r-5"></i> View</a>
-                                                                    <a class="dropdown-item" href="#"><i class="fa fa-file-pdf-o m-r-5"></i> Download</a>
-                                                                    <a class="dropdown-item" href="#"><i class="fa fa-trash-o m-r-5"></i> Delete</a>
-                                                                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_invoice"><i class="fa fa-trash-o m-r-5"></i> Delete</a>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>3</td>
+                                                            <td><a href="invoice-view.html">#INV-0003</a></td>
+                                                            <td>Dennis Salazar</td>
+                                                            <td>1 Sep 2018</td>
+                                                            <td>7 Sep 2018</td>
+                                                            <td>Tunai</td>
+                                                            <td>$20</td>
+                                                            <td><span class="custom-badge status-orange">Partially Paid</span></td>
+                                                            <td class="text-right">
+                                                                <div class="dropdown dropdown-action">
+                                                                    <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
+                                                                    <div class="dropdown-menu dropdown-menu-right">
+                                                                        <a class="dropdown-item" href="edit-invoice.html"><i class="fa fa-pencil m-r-5"></i> Edit</a>
+                                                                        <a class="dropdown-item" href="invoice-view.html"><i class="fa fa-eye m-r-5"></i> View</a>
+                                                                        <a class="dropdown-item" href="#"><i class="fa fa-file-pdf-o m-r-5"></i> Download</a>
+                                                                        <a class="dropdown-item" href="#"><i class="fa fa-trash-o m-r-5"></i> Delete</a>
+                                                                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_invoice"><i class="fa fa-trash-o m-r-5"></i> Delete</a>
+                                                                    </div>
                                                                 </div>
-                                                            </div>
-                                                        </td>
-                                                    </tr>
-                                                </tbody>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
                                                 </table>
                                             </div>
                                         </div>
